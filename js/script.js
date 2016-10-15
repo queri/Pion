5bbc6a958fe467bdcdf5efb9ba2131fd7500ef9e
queri <querimoni@gmail.com>
2016-10-15 20:50:59 +0300

    working mobile-menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
       var window_size = $(window).width();
-        if(window_size >= 992) {
+        if(window_size >= 960) {
             $('#fullpage').fullpage({
         //Navigation
         menu: '#menu',
@@ -14,11 +14,32 @@ $(document).ready(function() {
         };
 
 // mobile-menu
-  // $('#toggler').on('click', function(e){
-  //     e.preventDefault();
-  //     $(this).toggleClass('main-nav__toggler--close');
-  //     $('.mobile-nav').toggleClass('mobile-nav--visible');
-  // });
+
+
+  $('#toggler').on('click', function(e){
+      e.preventDefault();
+      $(this).toggleClass('mobile-menu--close');
+      $('#main-nav').toggleClass('mobile-nav--active');
+      $('#studio-nav').removeClass('mobile-nav--active');
+      $('#collection-nav').removeClass('mobile-nav--active');
+  });
+
+  $('#studio-nav-link').on('click', function(e){
+    e.preventDefault();
+    $('#studio-nav').addClass('mobile-nav--active');
+  });
+
+  $('#collection-nav-link').on('click', function(e){
+    e.preventDefault();
+    $('#collection-nav').addClass('mobile-nav--active');
+  });
+
+  $('.mobile-nav__back-link').on('click', function(e){
+    e.preventDefault();
+    $('.mobile-nav--active').removeClass('mobile-nav--active');
+    $('#main-nav').addClass('mobile-nav--active');
+  });
+
 // gallery
   $('.preview').on('click', function(){
     var imgSRC = $(this).attr('src');
